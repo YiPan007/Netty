b6764d0361b8548c0fc51f09ce2a0be72ab38f53
masterYiPan <1041427084@qq.com>
2017-10-03 21:46:30 +0800

    first commit

---
+++ Nettydemo3/src/nettydemo3_分隔符和定常解码器的运用/EchoServer.java
@@ -0,0 +1,56 @@
+package nettydemo3_分隔符和定常解码器的运用;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.handler.codec.DelimiterBasedFrameDecoder;
+import io.netty.handler.codec.string.StringDecoder;
+import io.netty.handler.logging.LogLevel;
+import io.netty.handler.logging.LoggingHandler;
+
+public class EchoServer {
+	public void bind(int port) throws Exception {
+		EventLoopGroup bossgroup = new NioEventLoopGroup();
+		EventLoopGroup workgroup = new NioEventLoopGroup();
+		try {
+			ServerBootstrap b = new ServerBootstrap();
+			b.group(bossgroup,workgroup).channel(NioServerSocketChannel.class)
+										.option(ChannelOption.SO_BACKLOG, 1024)
+										.handler(new LoggingHandler(LogLevel.INFO))
+										.childHandler(new ChannelInitializer<SocketChannel>() {
+
+											@Override
+											protected void initChannel(SocketChannel ch) throws Exception {
+												ByteBuf delimiter = Unpooled.copiedBuffer("$_".getBytes());
+												ch.pipeline().addLast(new DelimiterBasedFrameDecoder(1024,delimiter));
+												ch.pipeline().addLast(new StringDecoder());
+												ch.pipeline().addLast(new EchoServerHandler());
+												
+											}
+										});
+			ChannelFuture f = b.bind(port).sync();
+			f.channel().closeFuture().sync();
+		} finally {
+			bossgroup.shutdownGracefully();
+			workgroup.shutdownGracefully();
+		}
+	}
+	public static void main(String[] args) throws Exception {
+		int port =8080;
+		try {
+			if(args!=null&& args.length>0) {
+			port = Integer.valueOf(args[0]);
+		}
+		} catch (Exception e) {
+			
+		}
+		new EchoServer().bind(port);
+	}
+}
